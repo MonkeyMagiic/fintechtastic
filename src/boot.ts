d847b628cc6a4de1076d05077d4c1d751894bb0c
Tyrone Neill <tyrone_neill@yahoo.co.uk>
2015-12-30 23:44:50 +0100

    adding dashboard configuration and systemjs setup

---
+++ src/boot.ts
@@ -0,0 +1,5 @@
+import {bootstrap}    from 'angular2/platform/browser';
+import {Dashboard} from './dashboard/Dashboard';
+
+bootstrap(Dashboard);
+
---
+++ src/dashboard/Dashboard.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'dashboard',
+  template: '<h1>The Dashboard</h1>'
+})
+export class Dashboard {
+
+}
